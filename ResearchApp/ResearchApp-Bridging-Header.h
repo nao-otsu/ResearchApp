ed319fc0b7cd46cbb64f5b679f79044b65e6c533
夏山聡史 <satoshi@satoshi-2.local>
2017-05-23 20:25:11 +0900

    Add EZAudio Library

---
+++ ResearchApp/ResearchApp-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  ResearchApp-Bridging-Header.h
+//  ResearchApp
+//
+//  Created by 夏山聡史 on 2017/05/22.
+//  Copyright © 2017年 夏山聡史. All rights reserved.
+//
+
+#ifndef ResearchApp_Bridging_Header_h
+#define ResearchApp_Bridging_Header_h
+
+#import "EZAudio/EZAudio.h"
+
+#endif /* ResearchApp_Bridging_Header_h */
